20d3a2ccbd62305ffc26d017873e02d8213d3470
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-07 23:19:28 +0100

    feat: initials extract

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -2,5 +2,9 @@ let count = 0;
 
 count = count + 1;
 
+let initials = `${firstName[0]}${middleName[0]}${lastName[0]}`;
+
+console.log(initials);
+
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
