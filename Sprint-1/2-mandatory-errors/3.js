99941550cd3411b72578e3ed01d96d7ead144aaa
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-07 23:59:10 +0100

    fix: resolve syntax and logic errors in mandatory exercises 0-4

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
+console.log(last4Digits);
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,14 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const twelveHourClockTime = "08:53 PM";
+const twentyFourHourClockTime = "20:53";
+
+// The twelveHourClockTime variable should store the time in 12 hour clock format
+// The twentyFourHourClockTime variable should store the time in 24 hour clock format
+// However, the code isn't working
+// Before running the code, make and explain a prediction about why the code won't work
+// Then run the code and see what error it gives.
+// Consider: Why does it give this error? Is this what I predicted? If not, what's different?
+// Then try updating the variable declarations to get the correct values stored in each variable    
+
+console.log(`12h: ${twelveHourClockTime}`);
+console.log(`24h: ${twentyFourHourClockTime}`);
+
