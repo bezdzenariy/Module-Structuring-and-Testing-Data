d04feecd624afa71bb6d86a4d31e0d53fc6dc652
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-08 10:22:47 +0100

    fix: resolve return statement and variable scope bugs in debug exercises

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
-// Predict and explain first...
+// Predict and explain first... 
 
 // =============> write your prediction here
+  // result of multiplying 320 after that result of multiplying 10 and 32 is undefined
 
+//  function multiply(a, b) {
+//  console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> write your explanation here
+// no return so function loading the void
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,23 +2,36 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// num 103 is declared as a constant variable. function returns last digit of num by converting it to a string and slicing the last character.
+//const num = 103;
 
-const num = 103;
+//function getLastDigit() {
+//  return num.toString().slice(-1);
+//}
 
-function getLastDigit() {
+//console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+//console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+//console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+//The last digit of 42 is 3
+//The last digit of 105 is 3
+//The last digit of 806 is 3
+// Explain why the output is the way it is
+// =============> write your explanation here
+// getLastDigit() is empty so getLastDigit(42) ignore 42 and looking out found const num = 103 returning 3 every time
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
