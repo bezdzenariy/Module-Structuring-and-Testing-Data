80a02c29bb1a908ffbb34e3679c2ada8b211dc57
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-08 11:06:19 +0100

    feat: implement BMI, snake_case, and currency conversion functions

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,13 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function convertToUpperSnakeCase(text) {
+    //return text.replace(/ /g, '_').toUpperCase();
+    return text.replaceAll(" ", "_").toUpperCase();
+}
+
+console.log(convertToUpperSnakeCase("hello there")); // Output: "HELLO_THERE"
+console.log(convertToUpperSnakeCase("lord of the rings")); // Output: "LORD_OF_THE_RINGS"
+
+// This program defines a function called convertToUpperSnakeCase that takes a string input and converts it to UPPER_SNAKE_CASE format. The function uses the replaceAll method to replace all spaces in the input string with underscores, and then converts the entire string to uppercase using the toUpperCase method. Finally, it returns the modified string. The console.log statements demonstrate how the function works with different input strings.
