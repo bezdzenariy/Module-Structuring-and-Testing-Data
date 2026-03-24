642d69dd04c9220e3574933341288345bf60062a
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-24 15:23:33 +0100

    test: rewrite isProperFraction tests with Jest and cover edge cases

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/2-is-proper-fraction.test.js
@@ -6,8 +6,23 @@ test("should return true for a proper fraction", () => {
   expect(isProperFraction(2, 3)).toEqual(true);
 });
 
-// Case 2: Identify Improper Fractions:
+test("should return false for an improper fraction", () => {
+  expect(isProperFraction(5, 2)).toEqual(false);
+});
 
-// Case 3: Identify Negative Fractions:
+test("should evaluate absolute values for negative fractions", () => {
+  expect(isProperFraction(-4, 7)).toEqual(true);
+  expect(isProperFraction(4, -6)).toEqual(true);
+});
 
-// Case 4: Identify Equal Numerator and Denominator:
+test("should return false when numerator and denominator are equal", () => {
+  expect(isProperFraction(3, 3)).toEqual(false);
+});
+
+test("should return true when numerator is zero", () => {
+  expect(isProperFraction(0, 5)).toEqual(true);
+});
+
+test("should return false when denominator is zero (division by zero)", () => {
+  expect(isProperFraction(5, 0)).toEqual(false);
+});
