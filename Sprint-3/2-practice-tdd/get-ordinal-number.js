9a4f6d0144841574844f3845f4e8c9b978525991
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-27 16:13:39 +0100

    feat: complete Practice TDD exercises

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,26 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  //
+  const lastTwoDigits = num % 100;
+  //
+  const lastDIgit = num % 10;
+
+  // 
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return num + "th";
+  }
+
+  //
+  switch (lastDIgit) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
+
 
 module.exports = getOrdinalNumber;
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -8,6 +8,30 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // When the number is 1,
 // Then the function should return "1st"
 
-test("should return '1st' for 1", () => {
+test("should return 'st' for numbers ending in 1 (except 11)", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(101)).toEqual("101st");
+});
+
+test("should return 'nd' for numbers ending in 2 (except 12)", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(22)).toEqual("22nd");
+});
+
+test("should return 'rd' for numbers ending in 3 (except 13)", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+});
+
+test("should return 'th' for numbers ending in 4-9 (except 0)", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+  expect(getOrdinalNumber(10)).toEqual("10th");
+});
+
+test("should return 'th' for exceptions 11, 12, and 13", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(12)).toEqual("12th");
+  expect(getOrdinalNumber(13)).toEqual("13th");
+  expect(getOrdinalNumber(111)).toEqual("111th");
 });
