213dc8e0be6951a4afa05025b53fc4dc650054d4
Nik Bezdzenariy <kartoshkinkapustin@gmail.com>
2026-03-27 14:58:10 +0100

    feat: implement countChar function and complete TDD test suite

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,5 +1,13 @@
 function countChar(stringOfCharacters, findCharacter) {
-  return 5
+  let count = 0; // 
+
+  // iterate over each character in the passed string
+  for (const char of stringOfCharacters) {
+    if (char === findCharacter) {
+      count++; // if the symbol matches the one we are looking for, we increase the counter by 1
+    }
+  }
+  return count; // return the final value
 }
 
 module.exports = countChar;
--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -22,3 +22,20 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+// scenario: normal word
+test("should correctly count characters in a standard word", () => {
+  expect(countChar("hello", "l")).toEqual(2);
+  expect(countChar("javascript", "a")).toEqual(2);
+});
+
+// scenario: no occurrences
+test("should return 0 when the character is not found", () => {
+  expect(countChar("hello", "z")).toEqual(0);
+});
+
+// scenario: case sensitivity
+test("should be case-senitive", () => {
+  expect(countChar("Hello", "h")).toEqual(0);
+});
+
